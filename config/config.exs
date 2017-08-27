d77629df303c45b73ec1ac93a0ecb46acc4cd8fe
Vincent <vinh.nglx@gmail.com>
2017-08-27 18:24:19 +0800

    Database configuration

--- config/config.exs
+++ config/config.exs
@@ -28,3 +28,8 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+
+config :sjqut,
+  ecto_repos: [Sjqut.Repo]
+
+import_config "dev.local.exs"
