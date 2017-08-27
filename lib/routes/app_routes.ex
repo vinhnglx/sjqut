fac3f94809a9cc19759f22373a6abbbaa8a09afc
Vincent <vinh.nglx@gmail.com>
2017-08-27 17:42:48 +0800

    Solve the bug can't connect to favicon

--- lib/routes/app_routes.ex
+++ lib/routes/app_routes.ex
@@ -1,10 +1,17 @@
 defmodule Sjqut.AppRoutes do
   use Plug.Router
 
+  # https://stackoverflow.com/questions/32543265/how-to-configure-the-plug-static-without-phoenix/32544252#32544252
+  plug Plug.Static, at: "/", from: "lib/static", gzip: false, only: ~w(favicon.ico)
+
   plug :match
   plug :dispatch
 
   get "/about" do
     send_resp(conn, 200, "A side-project made by Vincent Nguyen - vinhnglx")
+  end
+
+  get "/" do
+    Sjqut.QuoteController.index(conn)
   end
 end
